4486f50f616fba46fb908f21593071385fc18d7f
ray <rezzmarr@gmail.com>
2025-09-23 17:01:42 +0200

    add vercel analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSansFont = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default async function RootLayout({
         </main>
 
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
